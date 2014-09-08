0b15cf6b9a69faabf28dff2f6b6794a4b23bba10
Tim Henderson <tim.tadh@gmail.com>
2014-09-08 08:14:05 -0400

    changed how lexc prints out instructions
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- inst/inst.go
+++ inst/inst.go
@@ -43,6 +43,20 @@ func (self Inst) String() (s string) {
 	return
 }
 
+func (self Inst) Serialize() (s string) {
+	switch self.Op {
+	case CHAR:
+		s = fmt.Sprintf("CHAR   %d, %d", self.X, self.Y)
+	case SPLIT:
+		s = fmt.Sprintf("SPLIT  %v, %v", self.X, self.Y)
+	case JMP:
+		s = fmt.Sprintf("JMP    %v", self.X)
+	case MATCH:
+		s = "MATCH"
+	}
+	return
+}
+
 func (self InstSlice) String() (s string) {
 	s = "{\n"
 	for i, inst := range self {
