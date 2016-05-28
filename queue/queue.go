5f544d335e413bd41deacdf640a19e778b5fb33b
Tim Henderson <tim.tadh@gmail.com>
2016-05-28 14:47:42 -0400

    using stack for queue. so much faster
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- queue/queue.go
+++ queue/queue.go
@@ -1,33 +1,30 @@
 package queue
 
-import "container/list"
-
 type Queue struct {
-	list *list.List
-	set  map[uint32]bool
+	list []uint32
+	set  map[uint32]struct{}
 }
 
 func New() *Queue {
 	self := new(Queue)
-	self.list = list.New()
-	self.set = make(map[uint32]bool)
+	self.list = make([]uint32, 0, 10)
+	self.set = make(map[uint32]struct{})
 	return self
 }
 
-func (self *Queue) Empty() bool { return self.list.Len() <= 0 }
+func (self *Queue) Empty() bool { return len(self.list) <= 0 }
 
 func (self *Queue) Push(pc uint32) {
 	if _, ok := self.set[pc]; ok {
 		return
 	}
-	self.set[pc] = true
-	self.list.PushBack(pc)
+	self.set[pc] = struct{}{}
+	self.list = append(self.list, pc)
 }
 
 func (self *Queue) Pop() uint32 {
-	e := self.list.Front()
-	pc, _ := e.Value.(uint32)
-	self.list.Remove(e)
+	pc := self.list[len(self.list)-1]
+	self.list = self.list[:len(self.list)-1]
 	delete(self.set, pc)
 	return pc
 }
